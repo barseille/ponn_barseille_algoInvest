646796f5141a6f4266533bb0cce4febe83d13977
BarseillePonn <ponn.barseille@gmail.com>
2023-04-15 12:10:35 +0200

    optimized mise en place

--- brute_force.py
+++ brute_force.py
@@ -1,6 +1,22 @@
+from dataclasses import dataclass
 from itertools import combinations
-from action import Action
 import csv
+
+@dataclass
+class Action:
+    nom: str
+    prix: float
+    pourcent: float
+    # calcul du profit
+    def profit(self):
+        return self.prix * self.pourcent / 100
+
+    def __str__(self):
+        return f"nom : {self.nom}, prix : {self.prix}, pourcent : {self.pourcent}%, profit : {self.profit():.2f}"
+
+    # Comparaison entre deux instances
+    def __lt__(self, autre_action):
+        return self.pourcent < autre_action.pourcent
 
 
 def recup_action_csv(nom_fichier):
@@ -8,26 +24,27 @@ def recup_action_csv(nom_fichier):
         data = csv.reader(f, delimiter=",")
         actions = []
         
+        
         # Lire la première ligne pour vérifier si elle contient des en-têtes
         headers = None
         try:
             headers = next(data)
         except StopIteration:
             pass
-        
+
         for row in data:
             nom = row[0]
             prix = float(row[1])
             benefice = float(row[2])
-            
+
             if prix <= 0.0 or benefice <= 0.0:
                 continue
-            
+
             action = Action(nom, prix, benefice)
             actions.append(action)
-            
+
         return actions
-    
+
 def trouver_meilleure_combinaison(actions, budget):  
     """Trouve la meilleure combinaison d'actions qui maximise le profit dans le budget donné"""
     
@@ -42,20 +59,17 @@ def trouver_meilleure_combinaison(actions, budget):
     meilleur_profit = 0
     investissement_total = 0
     
-    # Pour chaque combinaison possible, on calcule le total du prix des actions dans la combinaison
+    """ Pour chaque combinaison, on fait le total du prix des actions"""
     for combinaison in combinaisons:
-        total_prix = sum([action.prix for action in combinaison])
-        total_prix = 0  
-        for action in combinaison:
-            total_prix += action.prix
+        total_prix = sum([action.prix for action in combinaison])   
         
         # on vérifie si le total est inférieur ou égal au budget.
         if total_prix <= budget: 
             total_profit = sum([action.profit() for action in combinaison])
             
             """ 
-            Si oui, on calcule le profit total de cette combinaison 
-            et on vérifie si le profit total est supérieur au meilleur profit.
+            On vérifie si le profit total est supérieur au profit total actuel dans la boucle.
+            Si c'est le cas, on met à jour le meilleur profit
             """
             if total_profit > meilleur_profit:
                 meilleure_combinaison = combinaison
@@ -64,16 +78,11 @@ def trouver_meilleure_combinaison(actions, budget):
                         
     return (meilleure_combinaison, investissement_total)
 
-def obtenir_liste_achat_actions(meilleure_combinaison):
-    print("Voici la liste des actions à acheter : ")
-    liste_achat_actions = ", ".join([action.nom for action in meilleure_combinaison])
-    return liste_achat_actions
+def obtenir_liste_achat_actions():
+    liste_achat_actions = [action.nom for action in meilleure_combinaison]
+    return f"Voici la liste des actions à acheter : {liste_achat_actions}"
 
-    # liste_achat_actions = []
-    # for action in meilleure_combinaison:
-    #     liste_achat_actions.append(action.nom)
-
-def obtenir_profit_total(meilleure_combinaison):
+def obtenir_profit_total():
     profit = sum(action.profit() for action in meilleure_combinaison)
     """ :.2f = float de deux chiffres après la virgule """
     profit_total = f"Profit total sur 2 ans : {profit :.2f} euros"
@@ -82,12 +91,27 @@ def obtenir_profit_total(meilleure_combinaison):
 # Récupération des données à partir du fichier CSV
 actions = recup_action_csv("data/action.csv")
 
-
 # Recherche de la combinaison d'actions qui maximise le profit dans le budget de 500 euros
 meilleure_combinaison, investissement_total = trouver_meilleure_combinaison(actions, 500)
 
-print("Meilleure combinaison d'actions avec un budget de 500 euros : ")
-print(obtenir_liste_achat_actions(actions))
-print(obtenir_profit_total(actions))
+print("Meilleure combinaison d'actions avec un budget de 500 : ")
+print(obtenir_liste_achat_actions())
+print(obtenir_profit_total())
 print(f"Investissement total : {investissement_total:.2f} euros")
 
+
+
+
+
+
+
+# test = ["a", "b", "c", "d"]
+# test_possible = []
+# for i in range(1, len(test) + 1):
+#     test_possible.extend(combinations(test, i))
+# print(test_possible)
+# [('a',), ('b',), ('c',), ('d',), 
+# ('a', 'b'), ('a', 'c'), ('a', 'd'), ('b', 'c'), ('b', 'd'), ('c', 'd'), ('a', 'b', 'c'), 
+# ('a', 'b', 'd'), ('a', 'c', 'd'), ('b', 'c', 'd'), 
+# ('a', 'b', 'c', 'd')]
+  
--- optimized.py
+++ optimized.py
@@ -1,34 +1,42 @@
-import csv
-from action import Action, recup_action_csv
-from typing import List
+from brute_force import recup_action_csv
 
 
+# Récupération des actions depuis le fichier CSV
 actions1 = recup_action_csv("data/dataset1_Python+P7.csv")
+actions2 = recup_action_csv("data/dataset2_Python+P7.csv")
 
-        
-def glouton(actions: List[Action], budget: float) -> List[Action]:
-    # On trie les actions par profit décroissant
+def acheter_actions(budget_max, actions):
+    # Tri des actions par profit décroissant
     actions_triees = sorted(actions, reverse=True)
-    
-    
-    # On initialise la liste des actions à acheter
-    actions_achetees = []
-    
-    # On parcourt toutes les actions triées
-    for action in actions_triees:
-        # Si le prix ou le profit est à 0 ou en négatif, on passe à l'action suivante
-        if action.prix <= 0 or action.profit() <= 0:
-            continue
-        
-        # On calcule le nombre d'actions à acheter pour respecter le budget
-        nb_actions = int(budget / action.prix)
-        
-        
-        # Si le nombre d'actions est supérieur à 0, on les ajoute à la liste des actions achetées
-        if nb_actions > 0:
-            actions_achetees.append(action)
-            budget -= nb_actions * action.prix
-    
-    return actions_achetees
 
-# print(glouton(actions1, 500))
+    actions_achetees = []
+    budget_investi = 0
+
+    for action in actions_triees:
+        if action.prix <= budget_max - budget_investi:
+            actions_achetees.append(action)
+            budget_investi += action.prix
+
+    return actions_achetees, budget_investi
+
+# Acheter des actions pour chaque fichier CSV
+actions_achetees1, budget_investi1 = acheter_actions(500, actions1)
+actions_achetees2, budget_investi2 = acheter_actions(500, actions2)
+profit1 = sum(action.profit() for action in actions_achetees1)
+profit2 = sum(action.profit() for action in actions_achetees2)
+
+# Afficher les résultats pour chaque fichier CSV
+print("Résultats pour dataset1_Python+P7.csv :")
+print(f"Budget investi : {budget_investi1:.2f}")
+print(f"Profit total : {profit1:.2f}")
+print("Actions achetées : ")
+for action in actions_achetees1:
+    print(action.nom)
+
+print("Résultats pour dataset2_Python+P7.csv :")
+print(f"Budget investi : {budget_investi2:.2f}")
+print(f"Profit total : {profit2:.2f}")
+print("Actions achetées : ")
+for action in actions_achetees2:
+    print(action.nom)
+
