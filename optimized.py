b9cb431b8a584b6bcc87f104f8fc64c025691f20
BarseillePonn <ponn.barseille@gmail.com>
2023-04-20 12:02:40 +0200

    ajout fonction afficher_resultats

--- optimized.py
+++ optimized.py
@@ -1,9 +1,6 @@
 from brute_force import recup_action_csv, performance
 import tracemalloc
 
-# Récupération des actions depuis le fichier CSV
-actions1 = recup_action_csv("data/dataset1_Python+P7.csv")
-actions2 = recup_action_csv("data/dataset2_Python+P7.csv")
 
 @performance
 def acheter_actions(budget_max, actions):
@@ -12,46 +9,46 @@ def acheter_actions(budget_max, actions):
 
     actions_achetees = []
     budget_investi = 0
-
+    
     #  O(n) = boucle for
     for action in actions_triees:
         if action.prix <= budget_max - budget_investi:
             actions_achetees.append(action)
             budget_investi += action.prix
-
+    
     return actions_achetees, budget_investi
 
-
+def afficher_resultats(actions_achetees, budget_investi):
+    
+    liste = []
+    print(f"Budget investi : {budget_investi:.2f}")
+    print(f"Profit total : {sum(action.profit() for action in actions_achetees) :.2f}")
+    print("Actions achetées : ")
+    for action in actions_achetees:
+        liste.append(action.nom)
+    print(f"{liste}\n")
+    
+    
 def main():
+    
     tracemalloc.start()
     
-    # Acheter des actions pour chaque fichier CSV
-    liste1 = []
+    # Récupération des actions depuis le fichier CSV
+    actions1 = recup_action_csv("data/dataset1_Python+P7.csv")
+    actions2 = recup_action_csv("data/dataset2_Python+P7.csv")
+    
+    print("-"*40)
+    print("Résultats pour dataset1_Python+P7.csv :")
+    print("-"*40)
     actions_achetees1, budget_investi1 = acheter_actions(500, actions1)
-    profit1 = sum(action.profit() for action in actions_achetees1)
+    afficher_resultats(actions_achetees1, budget_investi1)
     
-    liste2 = []
-    actions_achetees2, budget_investi2 = acheter_actions(500, actions2)
-    profit2 = sum(action.profit() for action in actions_achetees2)
-
-    # Afficher les résultats pour chaque fichier CSV
-    print("\nRésultats pour dataset1_Python+P7.csv :")
-    print(f"Budget investi : {budget_investi1:.2f}")
-    print(f"Profit total : {profit1:.2f}")
-    print("Actions achetées : ")
-    for action in actions_achetees1:
-        liste1.append(action.nom)
-    print(f"{liste1}\n")
-
-    
+    print("-"*40)
     print("Résultats pour dataset2_Python+P7.csv :")
-    print(f"Budget investi : {budget_investi2:.2f}")
-    print(f"Profit total : {profit2:.2f}")
-    print("Actions achetées : ")
-    for action in actions_achetees2:
-        liste2.append(action.nom)
-    print(f"{liste2}\n")
-    
+    print("-"*40)
+    actions_achetees2, budget_investi2 = acheter_actions(500, actions2)
+    afficher_resultats(actions_achetees2, budget_investi2)
+
     # Obtenez la quantité de mémoire utilisée pendant l'exécution de la fonction
     memoire_courante, memoire_max = tracemalloc.get_traced_memory()
     print(f"Utilisation de mémoire courante : {memoire_courante / 10**6} MB")
